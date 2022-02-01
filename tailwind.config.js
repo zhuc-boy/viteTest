f205ab97c4e79f4be0705505730c2bc4850af44a
zhuc-boy <41360072+zhuc-boy@users.noreply.github.com>
2022-02-01 10:52:14 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        './src/**/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
